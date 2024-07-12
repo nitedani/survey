bfa67b122b0ca6608e20f11cf7535553442b54fb
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-07-12 02:33:23 +0200

    update

--- apps/website/src/server/index.ts
+++ apps/website/src/server/index.ts
@@ -1,22 +1,40 @@
 import express from 'express'
 import { renderPage } from 'vike/server'
-import { viteNode } from '@nitedani/vite-plugin-node/connect'
 import { requestContextMiddleware } from './cls'
+import httpDevServer from 'vavite/http-dev-server'
 
 startServer()
 
 async function startServer() {
   const app = express()
   app.use(requestContextMiddleware)
-  app.use(express.text({ limit: '10mb' }))
+  if (import.meta.env.PROD) {
+    app.use(express.static('client'))
+  }
 
-  app.use(
-    viteNode({
-      renderPage
-    })
-  )
+  app.get('*', async (req, res, next) => {
+    const pageContextInit = {
+      urlOriginal: req.originalUrl,
+      headersOriginal: req.headers
+    }
+    const pageContext = await renderPage(pageContextInit)
+    const { httpResponse } = pageContext
+    if (!httpResponse) {
+      return next()
+    } else {
+      const { statusCode, headers, earlyHints } = httpResponse
+      if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) })
+      headers.forEach(([name, value]) => res.setHeader(name, value))
+      res.status(statusCode)
+      httpResponse.pipe(res)
+    }
+  })
 
-  const port = process.env.PORT || 3000
-  app.listen(port)
-  console.log(`Server running at http://localhost:${port}`)
+  if (httpDevServer) {
+    httpDevServer!.on('request', app)
+  } else {
+    const port = process.env.PORT || 3000
+    app.listen(port)
+    console.log(`Server running at http://localhost:${port}`)
+  }
 }
--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -1,8 +1,9 @@
-import { viteNode } from '@nitedani/vite-plugin-node/plugin'
+import { standalone } from 'vite-plugin-standalone'
 import react from '@vitejs/plugin-react'
 import { join } from 'path'
 import vike from 'vike/plugin'
 import type { UserConfig } from 'vite'
+import { vavite } from 'vavite'
 
 export default {
   resolve: {
@@ -10,15 +11,31 @@ export default {
       '#root': join(__dirname, 'src')
     }
   },
+  buildSteps: [
+    { name: 'client' },
+    {
+      name: 'server',
+      config: {
+        build: { ssr: true }
+      }
+    }
+  ],
+  server: {
+    port: process.env.PORT && Number(process.env.PORT) || 3000,
+    host: '0.0.0.0',
+  },
   ssr: {
     noExternal: ['emoji-mart']
   },
   plugins: [
     react(),
-    vike(),
-    viteNode({
-      entry: './src/server/index.ts',
-      standalone: true
+    vike({ disableAutoFullBuild: true }),
+    vavite({
+      serverEntry: './src/server/index.ts',
+      serveClientAssetsInDev: true
+    }),
+    standalone({
+      entry: './src/server/index.ts'
     })
   ]
 } satisfies UserConfig
